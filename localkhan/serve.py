5e50d536e30508fa3968a2b1047c7bdd41e7cd61
Reto Aebersold <aeby@substyle.ch>
2015-06-13 11:23:32 -0600

    update lkhan to manually show sync progress

--- localkhan/serve.py
+++ localkhan/serve.py
@@ -15,7 +15,7 @@ from flask import Flask, send_from_directory
 from flask import render_template, make_response
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 LOCAL_KHAN_PATH = None
 
@@ -25,18 +25,7 @@ def set_khan_base_path(path):
     LOCAL_KHAN_PATH = path
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def root(path):
-    resp = make_response(app.send_static_file('index.html'))
-    resp.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    resp.headers["Pragma"] = "no-cache"
-    resp.headers["Expires"] = "0"
-    return resp
-
-
-@app.route('/khan.appcache')
-def manifest():
+def get_manifest():
     global LOCAL_KHAN_PATH
 
     assets = set()
@@ -45,9 +34,36 @@ def manifest():
         for file_name in files:
             assets.add(os.path.join(KHAN_CONTENT_STATIC, root_dir[len(LOCAL_KHAN_PATH) + 1:], file_name))
 
-    response = make_response(render_template('lkhan.appcache', assets=assets))
+    return render_template('lkhan.appcache', assets=assets)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def root(path):
+    # calculate manifest entries to show load progress
+    start = False
+    asset_count = 0
+    for line in get_manifest().split('\n'):
+        line = line.strip()
+        if line == 'CACHE:':
+            start = True
+        elif line == 'NETWORK:':
+            start = False
+        elif start and len(line) > 0:
+            asset_count += 1
+
+    resp = make_response(render_template('index.html', asset_count=asset_count))
+    resp.headers["Cache-Control"] = "no-cache, must-revalidate"
+    resp.headers["Pragma"] = "no-cache"
+    resp.headers["Expires"] = "0"
+    return resp
+
+
+@app.route('/khan.appcache')
+def manifest():
+    response = make_response(get_manifest())
     response.headers['Content-Type'] = 'text/cache-manifest'
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Cache-Control'] = 'no-cache, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
     return response
